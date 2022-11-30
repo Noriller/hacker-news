b40bca20910f2e3ebb6659c578c5ae4a364cc2b4
Bruno <brunonoriller@gmail.com>
2022-11-30 18:20:40 -0300

    initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,25 @@
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { type AppType } from "next/dist/shared/lib/utils";
+import React from "react";
+
+import "../styles/globals.css";
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  const [queryClient] = React.useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+};
+
+export default MyApp;
