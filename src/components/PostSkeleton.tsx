e02dd42e0e8732d7b513276c97f918d1bef57dfc
Bruno <brunonoriller@gmail.com>
2022-12-03 17:45:05 -0300

    feat: adding post skeleton

---
+++ src/components/PostSkeleton.tsx
@@ -0,0 +1,17 @@
+export function PostSkeleton() {
+  return (
+    <div className="container w-[70%] animate-pulse rounded-lg bg-gradient-to-r from-[#8029d688] to-[#ff008088] p-2">
+      <span className="sr-only">Loading...</span>
+      <div className="bg-black p-4">
+        <div className="mb-2 flex justify-between rounded-2xl border-2 border-white p-2 align-middle text-2xl">
+          <div className="m-2 h-3 w-48 rounded-full bg-gray-700" />
+        </div>
+        <div className="flex flex-col gap-2 rounded-2xl border border-gray-300 p-2 text-2xl">
+          <div className="mb-2.5 h-2 rounded-full bg-gray-700" />
+          <div className="mb-2.5 h-2 max-w-[320px] rounded-full bg-gray-700" />
+          <div className="mb-2.5 h-2 max-w-[360px] rounded-full bg-gray-700" />
+        </div>
+      </div>
+    </div>
+  );
+}
