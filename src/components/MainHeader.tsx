43c647a74545e1db37205c50c4f88f4cc99d00d7
Bruno <brunonoriller@gmail.com>
2022-12-03 17:45:29 -0300

    feat: adding header component

---
+++ src/components/MainHeader.tsx
@@ -0,0 +1,9 @@
+export const MainHeader = () => (
+  <h1
+    className="
+      bg-gradient-ellipse-tl from-[#ff9100] via-[#ffffff] to-[#efe81e] bg-clip-text
+      p-4 text-center text-5xl font-extrabold tracking-tight text-transparent sm:text-[5rem]"
+  >
+    Hacker News
+  </h1>
+);
