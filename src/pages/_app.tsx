8fdead7b98b813dc0d5bd918bd61e68818af338c
Bruno <brunonoriller@gmail.com>
2022-12-03 17:49:15 -0300

    feat: adding head to whole app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import {
 } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { type AppType } from "next/dist/shared/lib/utils";
+import Head from "next/head";
 import React from "react";
 
 import "../styles/globals.css";
@@ -16,6 +17,11 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={dehydratedState}>
+        <Head>
+          <title>Hacker News</title>
+          <meta name="description" content="Top stories from Hacker News" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
         <Component {...pageProps} />
       </Hydrate>
       <ReactQueryDevtools />
