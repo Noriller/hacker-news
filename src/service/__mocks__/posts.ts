cd5ca7a0c0fb279bab8083fd708be619ee108e00
Bruno <brunonoriller@gmail.com>
2022-12-12 09:39:12 -0300

    feat: adding more length to mock text

--- src/service/__mocks__/posts.ts
+++ src/service/__mocks__/posts.ts
@@ -8,11 +8,13 @@ export const makePostsMocks = (qty = 200) => {
   );
 };
 
+const randomBetween = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min;
+
 export const makePostMock = (id: number): Post => ({
   id,
   by: `post by ${id}`,
   score: id,
-  text: `text ${id}`,
+  text: `text ${id}  `.repeat(randomBetween(0, 100)),
   time: id * 10000000000,
   title: `title ${id}`,
   type: id % 2 == 0 ? 'story' : 'job',
